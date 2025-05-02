f75c02cb963bfc7a3ea1822f34c81d6d98292482
Bill Meyer <meyer8@llnl.gov>
2025-05-01 20:49:25 -0700

    Made the floats all pass by reference.

--- pythonwrappers.c
+++ pythonwrappers.c
@@ -10,7 +10,7 @@ bolom_core_fit_python_wrapper(shot,chans,nchans,inproj,sigma,kpsi,nkpsi,tension,
 {
   // Wraper by SRH so that the same routines can be called from OMFITprofiles using python
   // because of difficulties using the existing wrapper for IDL
-  bolomfit_core(*shot,chans,*nchans,inproj,sigma,kpsi,*nkpsi,*tension,efit, 
-		*rax,*zax,*rxpt1,*zxpt1,*rxpt2,*zxpt2,fitproj,fitimage,errorimage,fret); 
+  bolomfit_core(*shot,chans,*nchans,inproj,sigma,kpsi,*nkpsi,tension,efit, 
+		rax,zax,rxpt1,zxpt1,rxpt2,zxpt2,fitproj,fitimage,errorimage,fret); 
   return(0);
 }
